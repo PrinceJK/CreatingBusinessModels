b6b134864de8833d5b3eb62d685692bb12efa73f
Princejk <john.kehinde@decagon.dev>
2021-07-17 11:20:01 +0100

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CreatingBusinessModels
+{
+    public class Customer
+    {
+        private string lastName;
+        private string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
